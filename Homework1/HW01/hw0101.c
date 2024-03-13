d831f0073330a46c8cc7b720405f10350951787a
Darrin-Lin <darrin.de88@gmail.com>
2024-03-13 23:24:29 +0800

    feat: finish hw0101

--- Homework1/HW01/hw0101.c
+++ Homework1/HW01/hw0101.c
@@ -1,7 +1,22 @@
 #include "mystring.h"
-
+#include <string.h>
 int main()
 {
-
+    char s[] = "Hello, world!";
+    char sub[] = "wor";
+    printf("strchr: %s\n", strchr(s, 'w')); // "world!"
+    printf("mystrchr: %s\n", mystrchr(s, 'w')); // "world!"
+    printf("strrchr: %s\n", strrchr(s, 'o')); // "orld!"
+    printf("mystrrchr: %s\n", mystrrchr(s, 'o')); // "orld!"
+    printf("strspn: %zu\n", strspn(s, "Helo, wrd")); // 7
+    printf("mystrspn: %zu\n", mystrspn(s, "Helo, wrd")); // 7
+    printf("strcspn: %zu\n", strcspn(s, "Helo, wrd")); // 0
+    printf("mystrcspn: %zu\n", mystrcspn(s, "Helo, wrd")); // 0
+    printf("strpbrk: %s\n", strpbrk(s, "Helo, wrd")); // "ello, world!"
+    printf("mystrpbrk: %s\n", mystrpbrk(s, "Helo, wrd")); // "ello, world!"
+    printf("strstr: %s\n", strstr(s, sub)); // "world!"
+    printf("mystrstr: %s\n", mystrstr(s, sub)); // "world!"
+    printf("strtok: %s\n", strtok(s, ", ")); // "Hello"
+    printf("mystrtok: %s\n", mystrtok(s, ", ")); // "Hello"
     return 0;
 }
--- Homework1/HW01/mystring.c
+++ Homework1/HW01/mystring.c
@@ -1,7 +1,5 @@
-#include <stdio.h>
 #include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 
 
 char *mystrchr(const char *s, int c);
@@ -9,6 +7,110 @@ char *mystrrchr(const char *s, int c);
 size_t mystrspn(const char *s, const char *accept);
 size_t mystrcspn(const char *s, const char *reject);
 char *mystrpbrk(const char *s, const char *accept);
-char *mystrstr(const char *haystack , const char *needle);
+char *mystrstr(const char *haystack, const char *needle);
 char *mystrtok(char *str, const char *delim);
 
+char *mystrchr(const char *s, int c)
+{
+    while (*s != '\0')
+    {
+        if (*s == c)
+        {
+            return (char *)s;
+        }
+        s++;
+    }
+    return NULL;
+}
+char *mystrrchr(const char *s, int c)
+{
+    char *last = NULL;
+    while (*s != '\0')
+    {
+        if (*s == c)
+        {
+            last = (char *)s;
+        }
+        s++;
+    }
+    return last;
+}
+size_t mystrspn(const char *s, const char *accept)
+{
+    size_t count = 0;
+    while (*s != '\0')
+    {
+        if (mystrchr(accept, *s) == NULL)
+        {
+            return count;
+        }
+        count++;
+        s++;
+    }
+    return count;
+}
+size_t mystrcspn(const char *s, const char *reject)
+{
+    size_t count = 0;
+    while (*s != '\0')
+    {
+        if (mystrchr(reject, *s) != NULL)
+        {
+            return count;
+        }
+        count++;
+        s++;
+    }
+    return count;
+}
+
+char *mystrpbrk(const char *s, const char *accept)
+{
+    while (*s != '\0')
+    {
+        if (mystrchr(accept, *s) != NULL)
+        {
+            return (char *)s;
+        }
+        s++;
+    }
+    return NULL;
+}
+char *mystrstr(const char *haystack, const char *needle)
+{
+    size_t needle_len = strlen(needle);
+    while (*haystack != '\0')
+    {
+        if (strncmp(haystack, needle, needle_len) == 0)
+        {
+            return (char *)haystack;
+        }
+        haystack++;
+    }
+    return NULL;
+}
+char *mystrtok(char *str, const char *delim)
+{
+    static char *last = NULL;
+    if (str != NULL)
+    {
+        last = str;
+    }
+    if (last == NULL)
+    {
+        return NULL;
+    }
+    char *start = last;
+    while (*last != '\0')
+    {
+        if (mystrchr(delim, *last) != NULL)
+        {
+            *last = '\0';
+            last++;
+            return start;
+        }
+        last++;
+    }
+    last = NULL;
+    return start;
+}
