aa22b93e2b1d2ca856a841a5da8bf54d9c337feb
Darrin-Lin <darrin.de88@gmail.com>
2024-03-14 11:48:00 +0800

    update: update hw0102

--- Homework1/HW01/hw0102.c
+++ Homework1/HW01/hw0102.c
@@ -1,6 +1,10 @@
 #include "mycal.h"
 int main()
 {
+    uint64_t pass=0;
+    char * pExpr="ABC_10+-*";//error_case
 
+    sscanf(pExpr,"%*[0-9A-F_+-*]%n",&pass);
+    if(pass!=strlen(pExpr))printf("%d",pass);
     return 0;
 }
--- Homework1/HW01/mycal.c
+++ Homework1/HW01/mycal.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 // Input:
 // char *pExpr: the arithmetic expression.
 // int32_t base: the base that is used to show the arithmetic result. (2-16)
@@ -11,9 +10,28 @@
 // char **ppResult: the arithmetic result string.
 // Return:
 // 0: Success; -1: Error input
-int32_t calculate( char *pExpr , int32_t base, char **ppResult );
+int32_t calculate(char *pExpr, int32_t base, char **ppResult);
 
-static int32_t is_invalid( char *pExpr, int32_t base );
-static int32_t to_dec( char *pExpr, int32_t base );
-static char *to_base( int32_t dec, int32_t base );
+static int32_t is_invalid(char *pExpr, int32_t base);
+static int32_t to_dec(char *pNUm, int32_t base);
+static char *to_base(int32_t dec, int32_t base);
 
+static int32_t is_invalid(char *pExpr, int32_t base)
+{
+    char *pCheck = pExpr;
+    uint64_t pass = 0;
+    sscanf(pExpr, "%*[0-9A-F_+-*]%n", &pass);
+    if (pass != strlen(pExpr))
+        return -1;
+    // space not_same_advance
+}
+
+
+static int32_t to_dec(char *pNum, int32_t base)
+{
+
+}
+static char *to_base(int32_t dec, int32_t base)
+{
+    
+}
