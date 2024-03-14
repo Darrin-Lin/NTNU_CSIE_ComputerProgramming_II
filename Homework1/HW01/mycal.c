3439a6c88b604aedc2b7993c92d99c8a8697af24
Darrin-Lin <darrin.de88@gmail.com>
2024-03-15 00:10:12 +0800

    update: check operator

--- Homework1/HW01/mycal.c
+++ Homework1/HW01/mycal.c
@@ -3,6 +3,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+static int32_t is_invalid(char *pExpr, int32_t base);
+static int32_t to_dec(char *pNUm, int32_t base);
+static char *to_base(int32_t dec, int32_t base);
+
 // Input:
 // char *pExpr: the arithmetic expression.
 // int32_t base: the base that is used to show the arithmetic result. (2-16)
@@ -12,10 +16,6 @@
 // 0: Success; -1: Error input
 int32_t calculate(char *pExpr, int32_t base, char **ppResult);
 
-static int32_t is_invalid(char *pExpr, int32_t base);
-static int32_t to_dec(char *pNUm, int32_t base);
-static char *to_base(int32_t dec, int32_t base);
-
 static int32_t is_invalid(char *pExpr, int32_t base)
 {
     char *pCheck = pExpr;
@@ -24,14 +24,24 @@ static int32_t is_invalid(char *pExpr, int32_t base)
     if (pass != strlen(pExpr))
         return -1;
     // space not_same_advance
+    while (*pCheck != '\0')
+    {
+        if (*pCheck == '+' || *pCheck == '-' || *pCheck == '*')
+        {
+            if (*(pCheck + 1) == ' ')
+                return -1;
+            if ((pCheck != pExpr) && *(pCheck - 1) == ' ')
+                return -1;
+        }
+        pCheck++;
+    }
+    if(*pExpr == '+' || *pExpr == '-' || *pExpr == '*')
+        return -1;
 }
-
 
 static int32_t to_dec(char *pNum, int32_t base)
 {
-
 }
 static char *to_base(int32_t dec, int32_t base)
 {
-    
 }
