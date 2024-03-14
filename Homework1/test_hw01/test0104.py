9478609768da23bbac13c5ee5514f3a775419e17
Darrin-Lin <darrin.de88@gmail.com>
2024-03-14 11:28:58 +0800

    style: update test cases name start with HW -> hw

--- Homework1/test_hw01/test0104.py
+++ Homework1/test_hw01/test0104.py
@@ -2,6 +2,6 @@ import random as r
 HW=1
 HW_PART=1
 for i in range(0,10):
-    with open('./0'+str(HW_PART)+'/in/HW0'+str(HW)+'0'+str(HW_PART)+"_0"+str(i)+'.in', 'w') as f:
+    with open('./0'+str(HW_PART)+'/in/hw0'+str(HW)+'0'+str(HW_PART)+"_0"+str(i)+'.in', 'w') as f:
         f.write("")
         f.write('\n')
