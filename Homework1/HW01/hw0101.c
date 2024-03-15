466d5192bbaa332ed399dee735e427f066d41868
Darrin-Lin <darrin.de88@gmail.com>
2024-03-15 23:37:00 +0800

    test: update test program

--- Homework1/HW01/hw0101.c
+++ Homework1/HW01/hw0101.c
@@ -2,32 +2,95 @@
 #include <string.h>
 int main()
 {
-
-    char c[] = "123--23-52-5-35-25-45-45-23-5-235-24-5-5--436-3--------346-3-46-4-66-7-574-----6-2-652-5-25--24-6-34-6-3-63-6-2-64-246-52-2-624-6-26-2-6-26-24---2-6-4-62-6-24-634-64-36-45-64-64-56-4-64-64-6-46-46-4-64-64-64-64-";
-    char *a = "1-";
-    char C[] = "123--23-52-5-35-25-45-45-23-5-235-24-5-5--436-3--------346-3-46-4-66-7-574-----6-2-652-5-25--24-6-34-6-3-63-6-2-64-246-52-2-624-6-26-2-6-26-24---2-6-4-62-6-24-634-64-36-45-64-64-56-4-64-64-6-46-46-4-64-64-64-64-";
-
-    char *token = mystrtok(c, a);
-    char *Token = strtok(C,a);
-    printf("token %s",token);
-    // for (int i = 0; i < 212; i++)
-    //     printf("%c", *(c + i));
-    printf("\n");
-    while (token != NULL)
+    
+    while (1)
     {
-        // printf("%s\n", token);
-        for (int i = 0; i < 212; i++)
-            printf("%d", *(c + i)==*(C + i)?1:*(c+i));
-            printf("\n");   
-            for (int i = 0; i < 212; i++)
-            printf("%c", *(C + i));
-            printf("\n");
-            for (int i = 0; i < 212; i++)
-            printf("%c", *(c + i));
-            printf("\n");
-            Token=strtok(NULL,a);
-        token = mystrtok(NULL, a);
-        // printf("\n");
+        int type = 0;
+        char input[1000];
+        char inputcpy[1000];
+        scanf("%d", &type);
+        switch (type)
+        {
+        case 0:
+            return 0;
+        case -1:
+            printf("mystrchr\n");
+            printf("%s\n", mystrchr("abcdedf", 'e') == strchr("abcdedf", 'e') ? "true" : "false");
+            break;
+        case 1:
+            printf("mystrchr\n");
+            scanf("%s", input);
+            sscanf(input, "%s", inputcpy);
+            char c;
+            scanf(" %c", &c);
+            printf("%s\n", mystrchr(input, c) == strchr(inputcpy, c) ? "true" : "false");
+            break;
+        case -2:
+            printf("mystrrchr\n");
+            printf("%s\n", mystrrchr("abcdedf", 'e') == strrchr("abcdedf", 'e') ? "true" : "false");
+            break;
+        case 2:
+            printf("mystrrchr\n");
+            scanf("%s", input);
+            sscanf(input, "%s", inputcpy);
+            char c2;
+            scanf(" %c", &c2);
+            printf("%s\n", mystrrchr(input, c2) == strrchr(inputcpy, c2) ? "true" : "false");
+            break;
+        case -3:
+            printf("mystrpbrk\n");
+            printf("%s\n", mystrpbrk("abcdedf", "ed") == strpbrk("abcdedf", "ed") ? "true" : "false");
+            break;
+        case 3:
+            printf("mystrpbrk\n");
+            scanf("%s", input);
+            sscanf(input, "%s", inputcpy);
+            char c3[1000];
+            scanf(" %s", c3);
+            printf("%s\n", mystrpbrk(input, c3) == strpbrk(inputcpy, c3) ? "true" : "false");
+            printf("original: %s\n", input == inputcpy ? "true" : "false");
+            break;
+        case -4:
+            printf("mystrspn\n");
+            printf("%s\n", mystrspn("abcdedf", "ed") == strspn("abcdedf", "ed") ? "true" : "false");
+            break;
+        case 4:
+            printf("mystrspn\n");
+            scanf("%s", input);
+            sscanf(input, "%s", inputcpy);
+            char c4[1000];
+            scanf(" %s", c4);
+            printf("%s\n", mystrspn(input, c4) == strspn(inputcpy, c4) ? "true" : "false");
+            printf("original: %s\n", input == inputcpy ? "true" : "false");
+            break;
+        case -5:
+            printf("mystrcspn\n");
+            printf("%s\n", mystrcspn("abcdedf", "ed") == strcspn("abcdedf", "ed") ? "true" : "false");
+            break;
+        case 5:
+            printf("mystrcspn\n");
+            scanf("%s", input);
+            sscanf(input, "%s", inputcpy);
+            char c5[1000];
+            scanf(" %s", c5);
+            printf("%s\n", mystrcspn(input, c5) == strcspn(inputcpy, c5) ? "true" : "false");
+            printf("original: %s\n", input == inputcpy ? "true" : "false");
+            break;
+        case -6:
+            printf("mystrtok\n");
+            printf("%s\n", mystrtok("abcdedf", "ed") == strtok("abcdedf", "ed") ? "true" : "false");
+            break;
+        case 6:
+            printf("mystrtok\n");
+            scanf("%s", input);
+            sscanf(input, "%s", inputcpy);
+            char c6[1000];
+            scanf(" %s", c6);
+            printf("%s\n", mystrtok(input, c6) == strtok(inputcpy, c6) ? "true" : "false");
+            printf("original: %s\n", input == inputcpy ? "true" : "false");
+            break;
+        default:
+            break;
+        }
     }
-    printf("\n");
 }
