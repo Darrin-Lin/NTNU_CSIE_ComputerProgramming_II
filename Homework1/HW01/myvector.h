f8141d50ba0d23e86787bb20c9e6b32be1c12c68
Darrin-Lin <darrin.de88@gmail.com>
2024-03-16 18:00:50 +0800

    fix: fix vector function

--- Homework1/HW01/myvector.h
+++ Homework1/HW01/myvector.h
@@ -5,24 +5,24 @@
 
 int64_t *Vector_create(size_t size);
 void Vector_free(int64_t *pVector);
-int8_t Vector_push_back(int64_t **pVector, int64_t value);
-int8_t Vector_pop_back(int64_t **pVector);
-int8_t Vector_insert(int64_t **pVector, int64_t value, size_t index);
-int8_t Vector_erase(int64_t **pVector, size_t index);
+int8_t Vector_push_back(int64_t **pVector, int64_t value, size_t size);
+int8_t Vector_pop_back(int64_t **pVector, size_t size);
+int8_t Vector_insert(int64_t **pVector, int64_t value, size_t index, size_t size);
+int8_t Vector_erase(int64_t **pVector, size_t index, size_t size);
 int8_t Vector_resize(int64_t **pVector, size_t size);
 
 double *Vector_create_double(size_t size);
 void Vector_free_double(double *pVector);
-int8_t Vector_push_back_double(double **pVector, double value);
-int8_t Vector_pop_back_double(double **pVector);
-int8_t Vector_insert_double(double **pVector, double value, size_t index);
-int8_t Vector_erase_double(double **pVector, size_t index);
+int8_t Vector_push_back_double(double **pVector, double value, size_t size);
+int8_t Vector_pop_back_double(double **pVector, size_t size);
+int8_t Vector_insert_double(double **pVector, double value, size_t index, size_t size);
+int8_t Vector_erase_double(double **pVector, size_t index, size_t size);
 int8_t Vector_resize_double(double **pVector, size_t size);
 
-void **Vector_create_ptr(size_t size, size_t type_size);
+void **Vector_create_ptr(size_t size);
 void Vector_free_ptr(void **pVector);
-int8_t Vector_push_back_ptr(void ***pVector, void *value, size_t type_size);
-int8_t Vector_pop_back_ptr(void ***pVector);
-int8_t Vector_insert_ptr(void ***pVector, void *value, size_t index, size_t type_size);
-int8_t Vector_erase_ptr(void ***pVector, size_t index);
-int8_t Vector_resize_ptr(void ***pVector, size_t size, size_t type_size);
+int8_t Vector_push_back_ptr(void ***pVector, void *value, size_t size);
+int8_t Vector_pop_back_ptr(void ***pVector, size_t size);
+int8_t Vector_insert_ptr(void ***pVector, void *value, size_t index, size_t size);
+int8_t Vector_erase_ptr(void ***pVector, size_t index, size_t size);
+int8_t Vector_resize_ptr(void ***pVector, size_t size);
