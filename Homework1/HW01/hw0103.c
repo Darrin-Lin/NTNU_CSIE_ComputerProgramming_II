4d289f4ddb3956bd85106f1da88fa7892c248fba
Darrin-Lin <darrin.de88@gmail.com>
2024-03-17 22:05:09 +0800

    test: test hw0103.

--- Homework1/HW01/hw0103.c
+++ Homework1/HW01/hw0103.c
@@ -2,6 +2,17 @@
 
 int main()
 {
-
+    sPoly pFy = {1, (uint32_t[]){2}, (int32_t[]){1}};
+    sPoly pFx = {2, (uint32_t[]){1,0}, (int32_t[]){4,-3}};
+    sPoly pResult;
+    chain_rule(&pResult, &pFy, &pFx);
+    printf("pResult.size = %d\n", pResult.size);
+    for (uint32_t i = 0; i < pResult.size; i++)
+    {
+        printf("pResult.pPowers[%d] = %d\n", i, pResult.pPowers[i]);
+        printf("pResult.pCoefficients[%d] = %d\n", i, pResult.pCoefficients[i]);
+    }
+    free(pResult.pPowers);
+    free(pResult.pCoefficients);
     return 0;
 }
