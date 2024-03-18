929bbe33970ad99098a2ce44c6d97c32ba91a7c9
Darrin-Lin <darrin.de88@gmail.com>
2024-03-18 17:13:41 +0800

    fix: fix arrary

--- Homework1/HW01/mymaze.c
+++ Homework1/HW01/mymaze.c
@@ -31,64 +31,82 @@ typedef struct _sPath
 // If there is no path , return 0; If there is any errors in inputs , return -1; otherwise, return 1;
 int32_t find_min_path(const sRoom *pMaze, const uint8_t row, const uint8_t col, sPath *pMinPath);
 
-static int32_t find_path(uint8_t x, uint8_t y, int32_t *steps, const sRoom *pMaze, const uint8_t row, const uint8_t col);
+static int32_t find_path(int16_t x, int16_t y, int32_t *steps, const sRoom *pMaze, const uint8_t row, const uint8_t col);
 
-static int32_t find_path(uint8_t x, uint8_t y, int32_t *steps, const sRoom *pMaze, const uint8_t row, const uint8_t col)
+static int32_t find_path(int16_t x, int16_t y, int32_t *steps, const sRoom *pMaze, const uint8_t row, const uint8_t col)
 {
-    if (x < 0 || x >= row || y < 0 || y >= col)
+
+    if (x < 0 || x >= col || y < 0 || y >= row)
     {
         return -1;
     }
-    if (*(steps + x * col + y) != 0)
+    if (*(steps + y * row + x) != 0)
     {
-        return *(steps + x * col + y);
+        return *(steps + y * row + x);
     }
     if (x == 0 && y == 0)
     {
-        *(steps + x * col + y) = (pMaze + x * col + y)->cost;
-        return *(steps + x * col + y);
+        *(steps + y * row + x) = (pMaze + y * row + x)->cost;
+        return *(steps + y * row + x);
     }
     int32_t up = 0, down = 0, left = 0, right = 0;
-    if (y - 1 > 0 && (((pMaze + x * col + y)->doors & 0b11000000) >> 6) == (((pMaze + x * col + y - 1)->doors & 0b00001100) >> 2))
+    if (y - 1 > 0 && (((pMaze + y * row + x)->doors & 0b11000000) >> 6) == (((pMaze + (y - 1) * row + x)->doors & 0b00001100) >> 2))
     {
         up = find_path(x, y - 1, steps, pMaze, row, col);
     }
-    if (x + 1 < col && (((pMaze + x * col + y)->doors & 0b00110000) >> 4) == (((pMaze + (x + 1) * col + y)->doors & 0b00000011)))
+    if (x + 1 < col && (((pMaze + y * row + x)->doors & 0b00110000) >> 4) == (((pMaze + y * row + x + 1)->doors & 0b00000011)))
     {
         right = find_path(x, y + 1, steps, pMaze, row, col);
     }
-    if (y + 1 < row && (((pMaze + x * col + y)->doors & 0b00001100) >> 2) == (((pMaze + x * col + y + 1)->doors & 0b11000000) >> 6))
+    if (y + 1 < row && (((pMaze + y * row + x)->doors & 0b00001100) >> 2) == (((pMaze + (y + 1) * row + x)->doors & 0b11000000) >> 6))
     {
         down = find_path(x + 1, y, steps, pMaze, row, col);
     }
-    if (x - 1 > 0 && (((pMaze + x * col + y)->doors & 0b00000011)) == (((pMaze + (x - 1) * col + y)->doors & 0b00110000) >> 4))
+    if (x - 1 > 0 && (((pMaze + y * row + x)->doors & 0b00000011)) == (((pMaze + y * row + x - 1)->doors & 0b00110000) >> 4))
     {
         left = find_path(x - 1, y, steps, pMaze, row, col);
     }
     if (up == 0 && down == 0 && left == 0 && right == 0)
     {
-        *(steps + x * col + y) = -1;
+        *(steps + y * row + x) = -1;
         return -1;
     }
     int32_t min = up;
-    if (down != 0 && (min == 0 || down < min)&&down!=-1)
+    if (down != 0 && (min == 0 || down < min) && down != -1)
     {
         min = down;
     }
-    if (left != 0 && (min == 0 || left < min)&&left!=-1)
+    if (left != 0 && (min == 0 || left < min) && left != -1)
     {
         min = left;
     }
-    if (right != 0 && (min == 0 || right < min)&&right!=-1)
+    if (right != 0 && (min == 0 || right < min) && right != -1)
     {
         min = right;
     }
-    if(min==-1){
-        *(steps + x * col + y) = -1;
+    if (min == -1)
+    {
+        *(steps + y * row + x) = -1;
         return -1;
     }
-    *(steps + x * col + y) = min + (pMaze + x * col + y)->cost;
-    return min + (pMaze + x * col + y)->cost;
+    if (down != min && y - 1 > 0)
+    {
+        *(steps + (y - 1) * row + x) = -1;
+    }
+    if (right != min && x + 1 < col)
+    {
+        *(steps + y * row + x + 1) = -1;
+    }
+    if (down != min && y + 1 < row)
+    {
+        *(steps + (y + 1) * row + x) = -1;
+    }
+    if (left != min && x - 1 > 0)
+    {
+        *(steps + y * row + x - 1) = -1;
+    }
+    *(steps + y * row + x) = min + (pMaze + y * row + x)->cost;
+    return min + (pMaze + y * row + x)->cost;
 }
 int32_t find_min_path(const sRoom *pMaze, const uint8_t row, const uint8_t col, sPath *pMinPath)
 {
@@ -96,12 +114,12 @@ int32_t find_min_path(const sRoom *pMaze, const uint8_t row, const uint8_t col, 
     {
         return -1;
     }
-    int32_t steps[row][col];
+    int32_t steps[row * col];
     for (uint8_t i = 0; i < row; i++)
     {
         for (uint8_t j = 0; j < col; j++)
         {
-            steps[i][j] = 0;
+            steps[i * row + j] = 0;
         }
     }
     int64_t cost = find_path(row - 1, col - 1, steps, pMaze, row, col);
@@ -109,5 +127,5 @@ int32_t find_min_path(const sRoom *pMaze, const uint8_t row, const uint8_t col, 
     {
         return 0;
     }
-    
+    return 1;
 }
