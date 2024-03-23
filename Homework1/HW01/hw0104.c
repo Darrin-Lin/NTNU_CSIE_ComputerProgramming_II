6fd982f7db04897e59182fce808fae1a3c0f3c48
Darrin-Lin <darrin.de88@gmail.com>
2024-03-23 23:57:30 +0800

    fix: fixing stack-overflow but still error.

--- Homework1/HW01/hw0104.c
+++ Homework1/HW01/hw0104.c
@@ -1,13 +1,30 @@
 #include "mymaze.h"
 int main()
 {
-    sRoom a[4]={{1,0},{2,0},{1,0},{1,0}};
-    sPath b ;
-    printf("return: %d\n",find_min_path(a,2,2,&b));
-    printf("cost:%d length:%d\n\n",b.cost,b.length);
-    for(uint32_t i=0;i<b.length;i++)
+    int32_t row = 0, col = 0;
+    scanf("%d", &row);
+    scanf("%d", &col);
+    sRoom inp[row][col];
+    for (int32_t i = 0; i < row; i++)
     {
-        printf("%d %d\n",b.pPath[i].row,b.pPath[i].col);
+        for (int32_t j = 0; j < col; j++)
+        {
+            scanf("%u", &inp[i][j].cost);
+            // input doors in four sides
+            int32_t up, right, down, left;
+            scanf("%d %d %d %d", &up, &right, &down, &left);
+            inp[i][j].doors = (up << 6) | (right << 4) | (down << 2) | left;
+        }
     }
-    free(b.pPath);
-}   
+    sPath result;
+    result.pPath = NULL;
+    result.length = 0;
+    result.cost = 0;
+    printf("return: %d\n",find_min_path(&(inp[0][0]),row,col,&result));
+    printf("cost:%d length:%d\n\n",result.cost,result.length);
+    for(uint32_t i=0;i<result.length;i++)
+    {
+        printf("%d %d\n",result.pPath[i].row,result.pPath[i].col);
+    }
+    free(result.pPath);
+}
