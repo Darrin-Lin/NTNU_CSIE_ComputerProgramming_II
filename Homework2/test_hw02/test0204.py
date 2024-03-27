87f3a32b49a0ccc74313bae542600e618e88ab9e
Darrin-Lin <darrin.de88@gmail.com>
2024-03-28 00:06:10 +0800

    feat: create hw02 and finish hw0202.

---
+++ Homework2/test_hw02/test0204.py
@@ -0,0 +1,7 @@
+import random as r
+HW=2
+HW_PART=4
+for i in range(0,10):
+    with open('./0'+str(HW_PART)+'/in/hw0'+str(HW)+'0'+str(HW_PART)+"_0"+str(i)+'.in', 'w') as f:
+        f.write("")
+        f.write('\n')
