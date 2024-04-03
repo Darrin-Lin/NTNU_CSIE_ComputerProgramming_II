c0641e4b4d48b14b0fd360247cfa8fb431582df6
Darrin-Lin <darrin.de88@gmail.com>
2024-04-03 13:51:37 +0800

    docs: finish hw0205.

--- Homework2/HW02/hw0206.c
+++ Homework2/HW02/hw0206.c
@@ -1,9 +1,26 @@
 #include <errno.h>
 #include <stdio.h>
+#include <string.h>
 // define a new error number and use perror
 
 #define MY_ERRNO 200
 
+void perror(const char *s)
+{
+    if (s != NULL)
+    {
+        fprintf(stderr, "%s: ", s);
+    }
+    switch (errno)
+    {
+    case MY_ERRNO:
+        fprintf(stderr, "MY_ERRNO\n");
+        break;
+    default:
+        fprintf(stderr, "%s\n", strerror(errno));
+        break;
+    }
+}
 
 int main()
 {
