3fb3cad057eaf96c671702cadf626e1bae086c02
Darrin-Lin <darrin.de88@gmail.com>
2024-05-16 01:54:31 +0800

    feat: Add Debug message.

--- Homework4/HW04/hw0402.c
+++ Homework4/HW04/hw0402.c
@@ -3,6 +3,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+#ifdef DEBUG
+#define debug_print(fmt, ...)                       \
+    fprintf(stderr, "%s:%d:%s(): " fmt, __FILE__, \
+              __LINE__, __func__, ##__VA_ARGS__);
+#else
+#define debug_print(fmt, ...) \
+    {}
+#endif
+
 int main()
 {
 
