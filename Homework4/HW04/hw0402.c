58c49c92bd2dac4f32307920248e73257c127550
Darrin-Lin <darrin.de88@gmail.com>
2024-05-24 13:16:40 +0800

    fix: Fix $%* command injection.

--- Homework4/HW04/hw0402.c
+++ Homework4/HW04/hw0402.c
@@ -97,7 +97,7 @@ INPUT:
         goto INPUT;
         break;
     }
-    if(strchr(search,'"')||strchr(search,'\'')||strchr(search,';')||strchr(search,'}')||strchr(search,'{')||strchr(search,'\\'))
+    if(strchr(search,'"')||strchr(search,'\'')||strchr(search,';')||strchr(search,'}')||strchr(search,'{')||strchr(search,'\\')||strchr(search,'$')||strchr(search,'*')||strchr(search,'%'))
     {
         printf("ARE YOU TRYING COMMAND INJECTION???\n");
         return -1;
